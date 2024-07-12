1eab7dd1ed3d83410eaf0962e7dff866dc03ce9f
Nguyen Tran Thien Phu <23110286@student.hcmute.edu.vn>
2024-07-12 18:49:17 +0700

    được nha Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SCC_math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap chieu dai: ");
+            int ChieuDai = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Nhap chieu rong: ");
+            int ChieuRong = Convert.ToInt32(Console.ReadLine());
+            int DienTich = ChieuDai * ChieuRong;
+            Console.WriteLine("Dien tich hinh chu nhat la: " + DienTich);
+
+        }
+    }
+}
