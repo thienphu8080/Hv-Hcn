473fc70ada68f075f44520bce4b4441d819e27c5
thienphu8080 <ftry8080@gmail.com>
2024-07-13 07:12:28 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 ﻿using System.Numerics;
 ﻿namespace Code
 {
-    {internal class Program
+     internal class Program
+    {
         static void Main(string[] args)
         {
             Console.Write("Nhap chieu dai: ");
             int ChieuDai = Convert.ToInt32(Console.ReadLine());
-
             Console.Write("Nhap chieu rong: ");
             int ChieuRong = Convert.ToInt32(Console.ReadLine());
             int DienTich = ChieuDai * ChieuRong;
